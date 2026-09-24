2b88c3094fa8744e4792ae7ff0d60e8d17b70807
skrishna-code <krishnasutar026@gmail.com>
2026-09-24 09:24:26 +0530

    Add routing for Form, FormRef, and FormState components

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,34 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+import Form from "./Form";
+import FormRef from "./FormRef";
+import FormState from "./FormState";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link id="form-link" to="/">
+          Form
+        </Link>
+
+        <Link id="form-ref-link" to="/form-ref">
+          Form Ref
+        </Link>
+
+        <Link id="form-state-link" to="/form-state">
+          Form State
+        </Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/form-ref" element={<FormRef />} />
+        <Route path="/form-state" element={<FormState />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
